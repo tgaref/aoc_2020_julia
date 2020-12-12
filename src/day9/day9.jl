a5f7cd07dea1d27028d8fee254325076ce42ef7a
Theo Garefalakis <tgaref@gmail.com>
2020-12-12 10:42:43 +0200

    Refectored (and optimized) day9

--- src/day9/day9.jl
+++ src/day9/day9.jl
@@ -2,23 +2,13 @@ module Day9
 
 export day9a, day9b
 
-function update!(sums::Dict{Int,Int}, window, new)
-    out = window[1]
-    for number in window[2:end]
-        sums[out+number] -= 1
-        sums[new+number] = get(sums, new+number, 0) + 1
-    end
-    sums        
-end
-
-function initialize(window)
-    sums = Dict{Int,Int}()
-    @inbounds for i in 1:length(window)
-        @inbounds for j in i+1:length(window)
-            sums[window[i]+window[j]] = get(sums, window[i]+window[j], 0) + 1
+function issumoftwo(window, number)
+    for a in window
+        if in(number-a, window)
+            return true
         end
     end
-    sums
+    false
 end
 
 function day9a(filename, prelen)
@@ -26,14 +16,12 @@ function day9a(filename, prelen)
     for line in readlines(filename)
         push!(input, parse(Int, line))
     end
-    sums = initialize(input[1:prelen])
-    for i in 1:length(input)
-        new = input[prelen+i]
-        if get(sums, new, 0) == 0
-            return new
-        end
+    for i in 1:length(input)-prelen
         window = input[i:prelen+i-1]
-        update!(sums, window, new)
+        new = input[prelen+i]
+        if !issumoftwo(window, new)
+            return new
+        end        
     end    
 end
 
@@ -56,9 +44,10 @@ function day9b(filename, prelen)
     end
     target = day9a(filename, prelen)
     window = findwindow(input, target)
-    sort!(window)
-    window[1] + window[end]
+    (min, max) = reduce(window, init = (window[1],window[1])) do (min, max), n
+        (n < min ? n : min, n > max ? n : max)
+    end
+    min + max
 end
-
 
 end
