3f62b411b0115b0119af647267def6e703f48ee8
Theo Garefalakis <tgaref@gmail.com>
2020-12-13 14:01:44 +0200

    Added day 13

--- src/day13/day13.jl
+++ src/day13/day13.jl
@@ -2,12 +2,48 @@ module Day13
 
 export day13a, day13b
 
-function day13a(filename)
+using CombinedParsers
+import CombinedParsers.Regexp: word, whitespace_maybe
 
+function parseinput(filename)
+    input = Vector{String}()
+    for line in eachline(filename)
+        push!(input, line)
+    end
+    earliest = parse(Int, input[1])
+    buses = join(Repeat(Either('x', Numeric(Int))), ',')            
+    (earliest, input[2] |> buses)
 end 
 
-function day13b(filename)
+# Bus with timestamp t arrives at dt = l*t - earliest.
+# If earliest = k*t + r, then dt = (k+1)*t - earliest
+# = t - r = t - mod(earliest, t)
+function day13a(filename)
+    (earliest, buses::Vector{Union{Char,Int}}) = parseinput(filename)
+    buses = filter(buses) do x
+        isa(x, Int)
+    end
+    later = map(buses) do t
+        t - mod(earliest, t)
+    end
+    (dt,ind) = findmin(later)         
+    dt * buses[ind]
+end 
 
+# The time t is such that t = -i+1 mod n_i, where
+# n_i is the timestamp of the i-th bus in the list
+# including the 'x'. We solve using the CRT.
+function day13b(filename)
+    (earliest, buses::Vector{Union{Char,Int}}) = parseinput(filename)
+    N = prod(filter(c -> isa(c, Int), buses))
+    suma = 0
+    for (i,m) in enumerate(buses)
+        if isa(m, Int)
+            n = div(N,m)
+            suma = mod(suma + (n * (-i+1) * gcdx(n,m)[2]), N)
+        end
+    end
+    suma
 end 
 
 end
